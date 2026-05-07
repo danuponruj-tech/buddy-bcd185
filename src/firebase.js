d26d200b8bf2e9f634fa5de00b0b77a9ed3ff1e2
danuponruj-tech <danupon.ruj@gmail.com>
2026-05-08 00:03:56 +0700

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFWsztdGP_qlHv4ZIs0MTaSxQ_xPGbQUA",
+  authDomain: "buddy-system-3500f.firebaseapp.com",
+  projectId: "buddy-system-3500f",
+  storageBucket: "buddy-system-3500f.firebasestorage.app",
+  messagingSenderId: "921397744389",
+  appId: "1:921397744389:web:5cd708f4bbc166a664dd6c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
