86fd218378aa8460d55b12efcc9230128f56b0c0
danuponruj-tech <danupon.ruj@gmail.com>
2026-05-08 06:50:50 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+// ⚠️ แทนที่ด้วย Config จาก Firebase Console ของคุณ
 const firebaseConfig = {
   apiKey: "AIzaSyBFWsztdGP_qlHv4ZIs0MTaSxQ_xPGbQUA",
   authDomain: "buddy-system-3500f.firebaseapp.com",
@@ -13,5 +12,7 @@ const firebaseConfig = {
   appId: "1:921397744389:web:5cd708f4bbc166a664dd6c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
